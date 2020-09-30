bedff921ee93d658c38d008109b77d401ed4baa6
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2020-09-30 10:05:01 -0600

    After 9/28

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -13,8 +13,8 @@ import vs_script from "../shaders/vertex-color.glsl"
 import fs_script from "../shaders/fragment-color.glsl"
 
 // JavaScript
-import { scribble, displayMultiprogram } from './week05'
-displayMultiprogram()
+import { displayCube } from './week06'
+displayCube()
 
 
 
