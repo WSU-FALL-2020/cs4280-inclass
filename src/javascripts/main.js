34154f4cc5b69da1985fbf78309c4099d0db269f
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2020-10-28 07:11:02 -0600

    After 10/26

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -13,6 +13,6 @@ import vs_script from "../shaders/vertex-color.glsl"
 import fs_script from "../shaders/fragment-color.glsl"
 
 // JavaScript
-import { displayMeshes} from './week09'
-displayMeshes()
+import { displayTexturedScene} from './week10'
+displayTexturedScene()
 
